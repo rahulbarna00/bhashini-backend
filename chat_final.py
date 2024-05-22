6d013afd1b80084bee77ce5953b167506e9e4706
arif9353 <ma782318@gmail.com>
2024-05-23 01:29:16 +0530

    Updating the backend

--- chat_final.py
+++ chat_final.py
@@ -1,62 +1,38 @@
-import vanna as vn
-from dotenv import load_dotenv
-import os
-from vanna.remote import VannaDefault
-from IPython.display import Markdown
 import google.generativeai as genai
 from google.generativeai import GenerationConfig,GenerativeModel
 import pandas as pd
-from io import StringIO
+import os
+from dotenv import load_dotenv
 
-api_key = "8369b9bcaa5b41db8ee8e3c3e7ac9723"
-supa_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFzaXFydHF3a2NkcmpzeGdnZ21uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTM5ODE2MDksImV4cCI6MjAyOTU1NzYwOX0.HZGtXA5JHfcrvJ8puzfeStHD4utk7F37R8JJyElpt5o"
-url = "https://asiqrtqwkcdrjsxgggmn.supabase.co"
-host =  "aws-0-ap-south-1.pooler.supabase.com"
-name = "postgres"
-port = 5432
-user = "postgres.asiqrtqwkcdrjsxgggmn"
-password = "@ipragati1969"
-gemini_api = 'AIzaSyBd8WwvM1WpcN6ibodXo1zgjJLRKhYgXG0'
-genai.configure(api_key=gemini_api)
-model = genai.GenerativeModel("gemini-pro")
-
-vanna_model_name = "hisaaab"
-vn = VannaDefault(model=vanna_model_name, api_key=api_key)
-
-vn.connect_to_postgres(host=host,dbname=name,user=user,password=password,port=port)
-df_information_schema = vn.run_sql("SELECT * FROM INFORMATION_SCHEMA.COLUMNS")
-print(df_information_schema)
-df_inventory = df_information_schema[(df_information_schema['table_schema'] == 'public') & (df_information_schema['table_name'] == 'inventory')]
-print(df_inventory)
-plan = vn.get_training_plan_generic(df_inventory)
-print("success")
-vn.train(documentation= "The main aim of the system is to keep a track of the inventory systemically and generate visualizations according to the prompt in a hierarchial way.",
-         sql="SELECT * FROM inventory ORDER BY quantity DESC LIMIT 5;"
-         )
-
-def get_model_response(sql,df):
-    input_prompt = f"""Your query has returned the following result:
-                      sql query= {sql},
-                      dataframe = {df}.
-                      As a Customer-Friendly Chatbot, it's important to convey this information in a short, clear and helpful manner to the customer. Please generate a response accordingly and if unknown language is found, generate the response but don't translate that word.
-    """
-    generated_config = GenerationConfig(temperature=0.3)
-    response = model.generate_content(input_prompt,generation_config=generated_config)
-    generated_text = response.text
-    return generated_text
-
-def general_questions():
-    questions = vn.generate_questions()
-    return questions
-
-async def outputfn(question):
-    sql = vn.generate_sql(question)
-    print(sql)
-    frame = vn.run_sql(sql)
-    df = frame.to_string(index=False)
-    final_answer = get_model_response(sql,df)
-    # questions = vn.generate_followup_questions(question, sql, data)
-    # fig = vn.get_plotly_figure(plotly_code="fig = px.bar(df, title='Inventory Visualization')",df=df)
-    return final_answer
+load_dotenv()
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+genai.configure(api_key=GEMINI_API_KEY)
+model = genai.GenerativeModel("gemini-1.5-pro-latest")
 
 
+async def outputfn(question, data, language):
+    try:
+        main_data = pd.DataFrame(data)
+        prompt = f"""
+        Description:
+        The Inventory Management System is designed to track various aspects of inventory data including product details, pricing, quantities, user assignments, and more. Below is the dataframe of a dummy data for the inventory table:
+
+        Data: {main_data} where, \n 
+        Name : Name of the product,
+        Brand: Brand of the product,
+        Quantity: Quantity as in number of items in the product,
+        Threshold: Threshold of stock with respect to the product,
+        Category: Category of the product,
+        Net Weight: Net Weight of an individual product.
+        Price: Price of individual product\n
+        Answer to this query: {question} by going through the entire system in 1 sentence and don't generate anything else. Your response should be translated into this language: {language} without translating the values of the data.
+        """
+        generated_config = GenerationConfig(temperature=0.1)
+        response = model.generate_content(prompt,generation_config=generated_config)
+        generated_text = response.text
+        print(generated_text)
+        return generated_text
+    except Exception as e:
+        print(e)
+        return str(e)
+    
